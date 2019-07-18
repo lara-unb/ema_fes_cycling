8df67ae05a4c2f472762062cf1860237806d07c7
Lucas de Macedo <lucasdemacedo@lara.unb.br>
2019-07-18 09:33:48 -0300

    Ana's changes #1

--- scripts/control_node.py
+++ scripts/control_node.py
@@ -3,6 +3,7 @@
 import rospy
 import ema.modules.control as control
 import dynamic_reconfigure.client as reconfig
+import numpy as np
 
 # import ros msgs
 from sensor_msgs.msg import Imu
@@ -24,13 +25,32 @@ global speed_err
 global time
 global stim_current
 global stim_pw
+global n_cycles
+global cadence_cycle
+global mean_cadence
+global current_speed
+global add_max_current
+global add_current
+global control_on
+global min_vel
+global n_cycles
 
 on_off = False
+control_on = False
 angle = [0,0]
 speed = [0,0]
 speed_ref = 300
 speed_err = [0,0]
 time = [0,0]
+n_cycles = 0
+cadence_cycle = [0,0]
+mean_cadence = 0
+current_speed = 0
+add_max_current = 0
+add_current = 0
+min_vel = 0
+n_cycles = 0
+
 
 stim_current = {
     'Quad': {'Left': 0, 'Right': 0},
@@ -51,6 +71,10 @@ stim_order = ['Quad_Left','Quad_Right', # CH1 & CH2
 def server_callback(config):
     global stim_current
     global stim_pw
+    global add_max_current
+    global control_on
+    global add_current
+    global min_vel
 
     # assign updated server parameters to global vars 
     # refer to the server node for constraints
@@ -65,7 +89,20 @@ def server_callback(config):
             stim_current[muscle][side] = 0
             stim_pw[muscle][side] = 0
 
+    add_max_current = config['Add_current']
+    control_on = config['Simple_control']
+    min_vel = config['Min_vel']
+
+
 def pedal_callback(data):
+    global n_cycles
+    global cadence_cycle
+    global mean_cadence
+    global current_speed
+    global add_current
+    global add_max_current
+    global control_on
+    global min_vel
     # get timestamp
     time.append(data.header.stamp)
 
@@ -99,6 +136,38 @@ def pedal_callback(data):
     # print latest
     # print time[-1], angle[-1], speed[-1], speed_err[-1]
 
+    # update average cadence
+    if angle[-1] > angle[-2]:
+        current_speed = (angle[-1] - angle[-2]) * 50
+    else:
+        current_speed = 0
+
+    if current_speed > 1000:
+        current_speed = 0
+    cadence_cycle.append(current_speed)
+
+    # if current_speed < -1000:
+    #     current_speed = 0
+
+    if angle[-2] > 355:
+        if angle[-1] < 5:
+            n_cycles = n_cycles + 1
+            mean_cadence = np.mean(cadence_cycle)
+            cadence_cycle = []
+            km_rodados = (n_cycles*3.14159*1.5*66)/100000  #1.5: volta da coroa em relacao ao pneu/ 66cm = 26pol: tamanho do pneu/ 100000: cm->km/2*pi*D/2: comprimento do pneu
+            print n_cycles, mean_cadence, km_rodados
+
+            if control_on:
+                if mean_cadence < min_vel:
+                    add_current = add_current + 2
+
+                    if add_current > add_max_current:
+                        add_current = add_max_current
+            else:
+                add_current = 0
+
+            # print add_current
+
 def remote_callback(data):
     global stimMsg
     
@@ -131,6 +200,11 @@ def main():
     global stimMsg
     global stim_current
     global stim_pw
+    global mean_cadence
+    global current_speed
+    global add_max_current
+    global add_current
+    global n_cycles
 
     # init control node
     rospy.init_node('control', anonymous=False)
@@ -145,6 +219,8 @@ def main():
     # build basic angle/speed/signal message
     angleMsg = Float64()
     speedMsg = Float64()
+    cycle_speedMsg = Float64()
+    n_cyclesMsg = Float64()
     signalMsg = Int32MultiArray() # visual stimulator signal
     signalMsg.data = 7*[0] # [index] is the actual channel number
 
@@ -165,6 +241,8 @@ def main():
     pub['angle'] = rospy.Publisher('control/angle', Float64, queue_size=10)
     pub['signal'] = rospy.Publisher('control/stimsignal', Int32MultiArray, queue_size=10)
     pub['speed'] = rospy.Publisher('control/speed', Float64, queue_size=10)
+    pub['cycle_speed'] = rospy.Publisher('control/cycle_speed', Float64, queue_size=10)
+    pub['n_cycles'] = rospy.Publisher('control/n_cycles', Float64, queue_size=10)
 
     # define node loop rate (in hz)
     rate = rospy.Rate(50)
@@ -178,16 +256,25 @@ def main():
         for i, x in enumerate(stim_order):
             muscle = x[:4] # Quad, Hams or Glut
             side = x[5:] # Left or Right
+
+            # if stim_current[muscle][side] > 0:
+            #
+            #     stim_current[muscle][side] = stim_current[muscle][side] + add_current
+            #
+            #     if stim_current[muscle][side] > 80:
+            #         stim_current[muscle][side] = 80
+
             stimMsg.pulse_current[i] = round(stimfactors[i]*stim_current[muscle][side])
             stimMsg.pulse_width[i] = stim_pw[muscle][side]
-            signalMsg.data[i+1] = stimMsg.pulse_current[i] # [index] is the actual channel number
+            signalMsg.data[i+1] = stimMsg.pulse_current[i]# [index] is the actual channel number
 
         angleMsg.data = angle[-1]
 
-        current_speed = (angle[-1] - angle[-2])*50
-        if current_speed < -1000:
-            current_speed = 0
         speedMsg.data = current_speed
+        cycle_speedMsg.data = mean_cadence
+        n_cyclesMsg.data = n_cycles
+
+        print signalMsg.data
 
         # send stimulator update
         pub['control'].publish(stimMsg)
@@ -197,6 +284,12 @@ def main():
         pub['signal'].publish(signalMsg)
         # send speed update
         pub['speed'].publish(speedMsg)
+        # send cycle_speed update
+        pub['cycle_speed'].publish(cycle_speedMsg)
+        # send n_cycles update
+        pub['n_cycles'].publish(n_cyclesMsg)
+
+        # print add_max_current
 
         # wait for next control loop
         rate.sleep()
