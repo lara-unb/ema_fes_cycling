0abf4434f695e67ffe1c3634379ca135bd61640c
Gabriel Carmona <gabrielgcarmona@gmail.com>
2020-08-16 18:47:25 -0300

    Created Change imu service

--- scripts/services_node.py
+++ scripts/services_node.py
@@ -3,7 +3,9 @@
 import rospy
 import os
 import yaml
+import rospkg
 from std_srvs.srv import Trigger, TriggerResponse
+from ema_common_msgs.srv import imu, imuResponse
 
 def reboot_request(data):
     print("Reiniciando...")
@@ -13,12 +15,23 @@ def reboot_request(data):
         message="Rebooting"
     )
 
+def imu_change_request(data):
+    rospack = rospkg.RosPack()
+    imu_cfg_path=rospack.get_path("yostlabs_3space_imu")+"/config/imu.yaml"
+    with open(imu_cfg_path, 'r') as f:
+        imu_file = yaml.safe_load(f)
+    if data.imu_number > 0 and data.imu_number <= 10:
+        imu_file['wireless_id']['pedal'] = data.imu_number
+        with open(imu_cfg_path, 'w') as f:
+            yaml.safe_dump(imu_file, f)
+    return imuResponse(imu_file['wireless_id']['pedal'])
+
 def main():
     rospy.init_node('services') 
 
     # Create service reboot with response type Trigger and response function reboot_resquest
     reboot_service = rospy.Service('reboot', Trigger, reboot_request)
-    
+    imu_change_service = rospy.Service('changeImu',imu,imu_change_request)
     rospy.spin()
 
 if __name__ == '__main__':
